83db1baf79651440a9b842f45c88269f1c58ffdc
B22-user mir <mir.sharifullah2@gmail.com>
2022-03-23 19:41:14 -0400

    return method

---
+++ src/day34_Return_Type_Methode/VoidVSReturn.java
@@ -0,0 +1,10 @@
+package day34_Return_Type_Methode;
+
+public class VoidVSReturn {
+    public static void main(String[] args) {
+        sayhello();
+    }
+   public static void sayhello(){
+       System.out.println("Hello");
+   }
+}
